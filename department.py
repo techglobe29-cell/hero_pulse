49246eb3cb0d4f39fb325d5fe71dc476ed67c5fe
techglobe29-cell <techglobe29@gmail.com>
2026-06-15 09:20:04 +0530

    department.py

--- department.py
+++ department.py
@@ -3,11 +3,13 @@ from database import conn, cursor
 def add_department(name, location):
     sql = """
     INSERT INTO Department (Department_Name, Location)
-    VALUES (%s, %s)
+    VALUES (?, ?) 
     """
+    # Changed %s to ? for SQLite compatibility
     cursor.execute(sql, (name, location))
     conn.commit()
 
 def get_departments():
+    # This code was already correct!
     cursor.execute("SELECT Department_ID, Department_Name FROM Department")
     return cursor.fetchall()
