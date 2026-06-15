28c37e97dbbc21c1ec54603c589040b245a90b0d
techglobe29-cell <techglobe29@gmail.com>
2026-06-15 09:31:47 +0530

    maintenance.py

--- maintenance.py
+++ maintenance.py
@@ -2,9 +2,10 @@ from database import conn, cursor
 
 def add_maintenance(equipment_id, technician_id, mtype, scheduled_date, remarks):
     sql = """
-    INSERT INTO Maintenance
-    (Equipment_ID, Technician_ID, Maintenance_Type, Scheduled_Date, Status, Remarks)
-    VALUES (%s, %s, %s, %s, %s, %s)
+    INSERT INTO Maintenance 
+    (Equipment_ID, Technician_ID, Maintenance_Type, Scheduled_Date, Status, Remarks) 
+    VALUES (?, ?, ?, ?, ?, ?)
     """
+    # Changed %s to ? for SQLite compatibility
     cursor.execute(sql, (equipment_id, technician_id, mtype, scheduled_date, "Scheduled", remarks))
     conn.commit()
