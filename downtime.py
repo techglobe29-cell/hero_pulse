6e2174039cbb9f21d4e0e1c1c6f24d7992db26d4
techglobe29-cell <techglobe29@gmail.com>
2026-06-15 09:32:48 +0530

    downtime.py

--- downtime.py
+++ downtime.py
@@ -2,9 +2,10 @@ from database import conn, cursor
 
 def log_downtime(equipment_id, start_time, end_time, duration, failure_id, action_id):
     sql = """
-    INSERT INTO Downtime
-    (Equipment_ID, Start_Time, End_Time, Duration_Hours, Failure_ID, Action_ID)
-    VALUES (%s, %s, %s, %s, %s, %s)
+    INSERT INTO Downtime 
+    (Equipment_ID, Start_Time, End_Time, Duration, Failure_ID, Action_ID) 
+    VALUES (?, ?, ?, ?, ?, ?)
     """
+    # Changed %s to ? and updated Duration_Hours to Duration
     cursor.execute(sql, (equipment_id, start_time, end_time, duration, failure_id, action_id))
     conn.commit()
